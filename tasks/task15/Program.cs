b96b8b9a26881876d1364b731f27192d3f31e260
zhannas <zhannas@gmail.com>
2023-03-29 22:10:11 +0200

    task15

---
+++ tasks/task15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Entroduce el número de semana: ");
+int numberDay = int.Parse(Console.ReadLine());
+
+if (numberDay == 1)
+{
+    Console.WriteLine("Lunes, no es fin de semana!");
+}
+if (numberDay == 2)
+{
+    Console.WriteLine("Martes, no es fin de semana!");
+}
+if (numberDay == 3)
+{
+    Console.WriteLine("Miercoles, no es fin de semana!");
+}
+if (numberDay == 4)
+{
+    Console.WriteLine("Jueves, no es fin de semana!");
+}
+if (numberDay == 5)
+{
+    Console.WriteLine("Viernes, no es fin de semana!");
+}
+if (numberDay == 6)
+{
+    Console.WriteLine("Sabado, es fin de semana!");
+}
+if (numberDay == 7)
+{
+    Console.WriteLine("Domingo, es fin de semana!");
+}
