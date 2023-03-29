b3d0ab535e79dd7fdd1e5447a82892c2b2213c40
zhannas <zhannas@gmail.com>
2023-03-29 21:11:14 +0200

    task10

---
+++ tasks/task10/Program.cs
@@ -0,0 +1,7 @@
+﻿int randomNumber = new Random().Next(100, 1000);
+
+int twoDigitNumber = randomNumber / 10;
+int secondDigit = twoDigitNumber % 10;
+
+Console.WriteLine(randomNumber);
+Console.WriteLine(secondDigit);
